ff27a4f2482687ce6407ec771e8cc013cbadbe5c
ritli <agnar_oskarsson@hotmail.com>
2019-01-27 13:29:40 +0100

    Made  a randomizer

---
+++ Assets/FrameRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRandomizer : MonoBehaviour
+{
+	public Material[] mats;
+
+    void Start()
+    {
+		if (Random.value > 0.25f)
+		{
+			Destroy(gameObject);
+		}
+
+		GetComponent<MeshRenderer>().material = mats[Random.Range(0, mats.Length)];
+
+		transform.rotation *= Quaternion.Euler(0, 0, Random.Range(-10f, 10f));
+    }
+}
